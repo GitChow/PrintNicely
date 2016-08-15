7d3f7895fed192021063c1a4c425f64ccdf9af75
Joe <yaz.joe@hotmail.com>
2016-08-15 19:30:09 +0800

    better format the contact section

--- inject.js
+++ inject.js
@@ -1,21 +1,31 @@
+function createContactElement(name,content){
+    var contactElement = document.createElement("li");
+    var contactElementName = document.createElement("label");
+    var contactElementContent = document.createElement("span");
+    contactElementName.innerHTML = name;
+    contactElementContent.innerHTML = content;
+    contactElement.appendChild(contactElementName);
+    contactElement.appendChild(contactElementContent);
+    return contactElement
+}
+
 var contactUl = document.createElement("ul");
 contactUl.className = "contact-inserted"
 
 //deal with email
-var email=document.querySelector("#email li a").innerHTML
-// start to insert
-var emailLi = document.createElement("li");
-emailLi.innerHTML = "Email: "+email
-contactUl.appendChild(emailLi)
+var emailLink=document.querySelector("#email li a").innerHTML
+// insert
+var emailElement = createContactElement("Email:", emailLink)
+contactUl.appendChild(emailElement)
+
 
 //deal with linkedIn url
 var linkedinUrl=document.querySelector(".view-public-profile").href
-var linkedinLi = document.createElement("li");
-linkedinLi.innerHTML = "LinkedIn: "+linkedinUrl
-contactUl.appendChild(linkedinLi)
+var linkedInElement = createContactElement("LinkedIn:", linkedinUrl)
+contactUl.appendChild(linkedInElement)
+
 
 //deal with website Urls
-
 var allWebsiteUrls = document.querySelectorAll("#website-view a")
 for(var i=0;i<allWebsiteUrls.length;i++){
     var currentWebsite = allWebsiteUrls[i]
@@ -24,14 +34,11 @@ for(var i=0;i<allWebsiteUrls.length;i++){
     var webSiteUrlEncoded = currentWebsite.href
     var webSiteUrlDecoded = decodeURIComponent(webSiteUrlEncoded)
     var currentWebsiteUrl = webSiteUrlDecoded.split("url=")[1].split("&urlhash")[0]
-    console.info(currentWebsiteName+":"+currentWebsiteUrl)
 
-    // start to insert
-    var contactLi = document.createElement("li")
-    contactLi.innerHTML = currentWebsiteName + ": " + currentWebsiteUrl
-    contactUl.appendChild(contactLi)
+    //insert
+    var websiteElement = createContactElement(currentWebsiteName+": ", currentWebsiteUrl)
+    contactUl.appendChild(websiteElement)  
 }
-
 
 //start to append additional informaiton after the profile overview
 document.querySelector(".profile-card").appendChild(contactUl)
